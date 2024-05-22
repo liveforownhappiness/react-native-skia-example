95dfd5ba330cfd40dca56dfb0df35c8cf1830ead
liveforownhappiness <liveforownhappiness@gmail.com>
2024-05-22 15:04:47 +0900

    readme 추가

--- App.tsx
+++ App.tsx
@@ -5,12 +5,48 @@
  * @format
  */
 
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 
 import {Matrix} from './src/Matrix/Matrix';
+import {Text, TouchableOpacity, View} from 'react-native';
+import {Hue} from './src/Hue/Hue';
+
+type ScreenType = 'matrix' | 'hue';
 
 function App(): React.JSX.Element {
-  return <Matrix />;
+  const [screen, setScreen] = useState<ScreenType>('matrix');
+
+  const returnScreen = useCallback(() => {
+    if (screen === 'matrix') {
+      return <Matrix />;
+    }
+    if (screen === 'hue') {
+      return <Hue />;
+    }
+    return <Matrix />;
+  }, [screen]);
+
+  return (
+    <View style={{flex: 1}}>
+      {returnScreen()}
+      <View style={{width: '100%', height: 40, flexDirection: 'row'}}>
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          onPress={() => setScreen('matrix')}>
+          <Text>MATRIX</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+          onPress={() => setScreen('hue')}>
+          <Text>HUE</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 export default App;
